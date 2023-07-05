99c87f6490f0dfa29cde001656cb4f6c23f1e9b3
Guilherme Lopes Ferreira <gferreira525@gmail.com>
2023-07-05 12:46:09 -0300

    split steps component

---
+++ src/components/Steps.tsx
@@ -0,0 +1,99 @@
+import Question from "@/components/Question";
+import { Button, message, Steps as AntdSteps } from "antd";
+import { useState } from "react";
+
+const steps = [
+  {
+    title: "Question 1",
+    content: (
+      <Question
+        number={1}
+        alternatives={[
+          "alternative 1",
+          "alternative 2",
+          "alternative 3",
+          "alternative 4",
+        ]}
+      />
+    ),
+  },
+  {
+    title: "Question 2",
+    content: (
+      <Question
+        number={2}
+        alternatives={[
+          "alternative 1",
+          "alternative 2",
+          "alternative 3",
+          "alternative 4",
+        ]}
+      />
+    ),
+  },
+  {
+    title: "Question 3",
+    content: (
+      <Question
+        number={3}
+        alternatives={[
+          "alternative 1",
+          "alternative 2",
+          "alternative 3",
+          "alternative 4",
+        ]}
+      />
+    ),
+  },
+];
+
+export const Steps = () => {
+  const [current, setCurrent] = useState(0);
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+
+  const items = steps.map((item) => ({ key: item.title, title: item.title }));
+
+  const contentStyle: React.CSSProperties = {
+    lineHeight: "260px",
+    textAlign: "center",
+    color: "#000",
+    backgroundColor: "#fefefe",
+    borderRadius: 100,
+    border: `1px dashed #000`,
+    marginTop: 16,
+  };
+
+  return (
+    <>
+      <AntdSteps current={current} items={items} />
+      <div style={contentStyle}>{steps[current].content}</div>
+      <div style={{ marginTop: 24 }}>
+        {current < steps.length - 1 && (
+          <Button type="primary" onClick={() => next()}>
+            Next
+          </Button>
+        )}
+        {current === steps.length - 1 && (
+          <Button
+            type="primary"
+            onClick={() => message.success("Processing complete!")}
+          >
+            Done
+          </Button>
+        )}
+        {current > 0 && (
+          <Button style={{ margin: "0 8px" }} onClick={() => prev()}>
+            Previous
+          </Button>
+        )}
+      </div>
+    </>
+  );
+};
