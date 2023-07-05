ab79ce93b41ce9e07a6cbcc3acf5b725e564c7f4
Guilherme Lopes Ferreira <gferreira525@gmail.com>
2023-07-04 21:54:26 -0300

    next + antd

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Button from "antd/lib/button";
+import H1 from "antd/lib/typography/Title";
+
+export default function Home() {
+  return (
+    <div>
+      <H1>Home</H1>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
